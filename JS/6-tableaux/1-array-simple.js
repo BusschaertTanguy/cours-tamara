ad684629e34cb644cec8c813d73b45200210563f
Tanguy Busschaert <tanguy.busschaert.95@gmail.com>
2022-12-19 15:00:02 +0100

    resultat des excercices

--- JS/6-tableaux/1-array-simple.js
+++ JS/6-tableaux/1-array-simple.js
@@ -1,13 +1,29 @@
 // Ceci est 1 grand excercice qui travaille sur le meme array
 
 // a) Creer un array 'pays' avec les pays suivant : 'Belgique', 'France', 'Allemagne'.
-
+let pays = ['Belgique', 'France', 'Allemagne']
+console.log(pays)
 // b) Imprimer (log) le nombre d'elements dans l'array.
-
+let nombreDePays = pays.length;
+console.log(nombreDePays)
 // c) Modifier le 2ᵉ element de l'array vers 'Pays-bas', puis imprimer le.
-
+pays[1]='Pays-bas';
+console.log(pays)
 // d) Ajouter de nouveau le pays 'France' devant dans l'array, et retirer le dernier pays.
-
+pays.unshift('France');
+console.log(pays)
+let paysEnlever= pays.pop()
+console.log(paysEnlever)
+console.log(pays)
 // e) Trouver le pays 'Pays-bas', et le retirer.
+let paysPaysBas =pays.indexOf('Pays-bas');
+if (paysPaysBas !==-1){
+    let retirePaysBas= pays.splice(paysPaysBas,1);
+    console.log(retirePaysBas);
+}console.log(pays)
+
 
 // f) Finalement, utiliser une boucle pour imprimer chaque element.
+for(let paysBoucle of pays) {
+    console.log(paysBoucle);
+}
--- JS/6-tableaux/2-array-avance.js
+++ JS/6-tableaux/2-array-avance.js
@@ -1,9 +1,21 @@
 // Ceci est 1 grand excercice qui travaille sur le meme array
 
 // a) Creer un array avec les mots suivants 'Montre', 'Voiture', 'Ourse', 'Cuisine'
-
-// b) Utiliser la methode map qui ajoute un 's' à la fin de chaque mot.
-
+let objet=['Montre', 'Voiture', 'Ourse', 'Cuisine'];
+console.log(objet);
+// b) Utiliser la methode map qui ajoute un 's' à la fin de chaque mot
+function ajouteUnS(objet){
+    return objet+'s';
+}
+let objets=objet.map(ajouteUnS);
+console.log(objets)
 // c) Utiliser la methoder filter pour garder les qui sont plus grands que 6 lettres
-
+function plusLongQue6(mot) {
+    return mot.length > 6;
+}
+let motPlusLongQue6= objet.filter(plusLongQue6);
+console.log(motPlusLongQue6)
 // d) Convertir l'array vers un string delimiter par ';', et imprimer le result
+console.log(objet)
+let objetEnString = objet.join(',');
+console.log(objetEnString)
